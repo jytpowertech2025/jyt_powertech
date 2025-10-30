bf40194f7d184f557f8221c05998f2ac7873fa6c
khilonjiya3 <pankajdoleyapdcl@gmail.com>
2025-10-30 12:26:30 +0530

    Update HeroImage.jsx

--- src/components/HeroImage.jsx
+++ src/components/HeroImage.jsx
@@ -4,7 +4,7 @@ const HeroImage = () => {
   return (
     <div className='flex justify-center items-center'>
       <img 
-        src='https://imagedelivery.net/LqiWLm-3MGbYHtFuUbcBtA/119580eb-abd9-4191-b93a-f01938786700/public' 
+        src='' 
         alt='JYT' 
       />
     </div>
